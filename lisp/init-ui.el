091424caa2a547d07b866b4203bb8c8dbb615aa8
Ethanlinyf <e.yflin@gmail.com>
2019-07-28 14:38:39 +1000

    update init-ui(UI)

---
+++ lisp/init-ui.el
@@ -0,0 +1,12 @@
+
+(setq inhibit-splash-screen t)
+
+;; full-screen
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+(global-hl-line-mode t)
+
+;; load theme
+(load-theme 'monokai t)
+
+(provide 'init-ui)
