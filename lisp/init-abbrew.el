827eb734ad905baf835149bb5a3fe4401ec776a4
Ethanlinyf <e.yflin@gmail.com>
2019-07-28 18:56:15 +1000

    remove files based on .gitignore

--- lisp/init-abbrew.el
+++ lisp/init-abbrew.el
@@ -1,12 +1,7 @@
 
 (setq-default abbrev-mode t)
 (define-abbrev-table 'global-abbrev-table '(
-					    ;; signature
-					    ("3yf" "Yufeng Lin")
-					    ;; Microsoft
-					    ("3email" "yourslinyf@gmail.com")
-					    ("3address" "5 Kamaran Court, Annandale, Townsville QLD, 4814")
-					    ))
+					    					    ))
 
 
 (provide 'init-abbrew)
