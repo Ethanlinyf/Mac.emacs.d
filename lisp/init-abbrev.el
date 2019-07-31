8dc916dc8ffc422ebff41a16423e34bcee941f35
Ethanlinyf <e.yflin@gmail.com>
2019-07-31 19:31:57 +1000

    test again init-abbrev

--- lisp/init-abbrev.el
+++ lisp/init-abbrev.el
@@ -2,7 +2,11 @@
 (setq-default abbrev-mode t)
 (define-abbrev-table 'global-abbrev-table '(
 					    ;; signature
-					    					    ))
+					    ("3yf" "Yufeng Lin")
+					    ;; Microsoft
+					    ("3email" "yourslinyf@gmail.com")
+					    ("3address" "5 Kamaran Court, Annandale, Townsville QLD, 4814")
+					    ))
 
 
 (provide 'init-abbrev)
